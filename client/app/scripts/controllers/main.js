2cc47b0f9363f0ae9f82a5b4260e683e1df4bc21
Coridyn <coridyn@gmail.com>
2014-05-25 13:26:13 +1000

    Print flags to console

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -119,4 +119,8 @@ angular.module('clientApp').controller('MainCtrl', function($scope, $http, FlagS
 	$scope.setColor = function(row, col, color) {
 		$scope.flag[row][col] = color;
 	}
+	
+	$scope.logFlags = function(){
+		console.log(angular.toJson($scope.flags, true));
+	}
 });
